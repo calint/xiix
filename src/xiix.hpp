76a3599eab12c12118f144018929b4369b2e8dcc
calin tenitchi <calin.tenitchi@gmail.com>
2016-05-18 12:17:12 +0700

    .

--- src/xiix.hpp
+++ src/xiix.hpp
@@ -13,150 +13,6 @@ namespace xiix{
 
 // example from http://asistobe851.web.fc2.com/C_and_Cpp/epoll-client.html
 
-void
-setNonblocking ( int sock ) {
-    int flag = fcntl ( sock, F_GETFL, 0 );
-    fcntl ( sock, F_SETFL, flag | O_NONBLOCK );
-}
-
-int
-getSocket ( char *hostname, int portNumber ) {
-
-    struct sockaddr_in addr;
-    struct hostent *he;
-    int sock;
-
-    if ( hostname == NULL ) {
-        perror ( "host is undefined!" );
-        exit ( 1 );
-    }
-
-    if ( ( sock = socket ( AF_INET, SOCK_STREAM, 0 ) ) < 0 ) {
-        perror ( "socket" );
-        exit ( 2 );
-    }
-
-    bzero ( ( char * ) &addr, sizeof ( addr ) );
-
-    if ( ( he = gethostbyname ( hostname ) ) == NULL ) {
-        perror ( "No such host" );
-        exit ( 3 );
-    }
-    bcopy ( he->h_addr, &addr.sin_addr, he->h_length );
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons ( portNumber );
-
-    if ( connect ( sock, ( struct sockaddr * ) &addr, sizeof ( addr ) ) < 0 ) {
-        perror ( "connect" );
-        exit ( 4 );
-    }
-
-    return sock;
-}
-
-int
-main (int argc,char **argv ) {
-    char *host;
-    int port;
-    char *requestURI;
-    int i = 0;
-//    int maxClients = 0;
-
-    if ( argc != 5 ) {
-        fprintf ( stderr, "Usage: %s [host] [port] [requestURI] [maxClients]\n", argv[0] );
-        return -1;
-    }
-    host = argv[1];
-    port = atoi ( argv[2] );
-    requestURI = argv[3];
-//    maxClients = atoi(argv[4]);
-    const int maxClients = 64;
-
-    //--------------------------------------------------
-    // create epoll's descriptors
-    //--------------------------------------------------
-    int epfd;
-    if ( ( epfd = epoll_create ( maxClients ) ) < 0 ) {
-        perror ( "epoll_create" );
-        exit ( 1 );
-    }
-
-    //--------------------------------------------------
-    // create as many socket handles as needed
-    //--------------------------------------------------
-    int sock[maxClients];
-    for ( i = 0; i < maxClients; i++ ) {
-        sock[i] = getSocket ( host, port );
-        setNonblocking ( sock[i] );
-
-        fprintf ( stderr, "... socket handle created ... \n" );
-    }
-
-    //--------------------------------------------------
-    // add all the sockets into epoll
-    //--------------------------------------------------
-    struct epoll_event ev;
-    struct epoll_event events[maxClients];
-
-    for ( i = 0; i < maxClients; i++ ) {
-        memset ( &ev, 0, sizeof ev );
-        ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
-        ev.data.fd = sock[i];
-        if ( epoll_ctl ( epfd, EPOLL_CTL_ADD, sock[i], &ev ) < 0 ) {
-            fprintf ( stderr, "epoll_ctl()\n" );
-            exit ( 2 );
-        }
-    }
-
-    //--------------------------------------------------------------
-    // wait for the available status of each epoll descriptor
-    //--------------------------------------------------------------
-    int nfd;
-    while ( 1 ) {
-        nfd = epoll_wait ( epfd, events, maxClients, -1 );
-
-        for ( i = 0; i < nfd; ++i ) {
-            fprintf ( stderr, "*** now polling *** i=%d\n", i );
-
-            int client = events[i].data.fd;
-            char message[1024];
-
-            if ( events[i].events & EPOLLOUT ) {
-                sprintf ( message, "GET %s HTTP/1.1\r\nHost: %s:%d\r\n\r\n",
-                          requestURI, host, port );
-                int write_len = write ( client, message, strlen ( message ) );
-                printf ( "write_len=%d\n", write_len );
-            }
-
-            if ( events[i].events & EPOLLIN ) {
-                memset ( message, 0, sizeof message );
-                int read_len = read ( client, message, sizeof message );
-                printf ( "read_len=%d\n", read_len );
-
-                // detached
-                epoll_ctl ( epfd, EPOLL_CTL_DEL, client, &ev );
-                // close socket
-                close ( client );
-
-                // add new descriptor
-                int newSock = getSocket ( host, port );
-                setNonblocking ( newSock );
-
-                memset ( &ev, 0, sizeof ev );
-                ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
-                ev.data.fd = newSock;
-                if ( epoll_ctl ( epfd, EPOLL_CTL_ADD, newSock, &ev ) < 0 ) {
-                    fprintf ( stderr, "epoll_ctl()\n" );
-                    exit ( 3 );
-                }
-            }
-
-        }
-    }
-    return 0;
-}
-
-
 class client{
 public:
 	client(int argc,char**argv,char**env){
@@ -165,5 +21,100 @@ public:
 			puts(*p++);
 		main(argc,argv);
 	}
+
+	int	main(int argc,char**argv ){
+		if(argc!=5){
+			fprintf(stderr,"Usage: %s [host] [port] [requestURI] [maxClients]\n",argv[0]);
+			return -1;
+		}
+		const char*host=argv[1];
+		const int port=atoi(argv[2]);
+		const char*requestURI=argv[3];
+		const int maxClients=1;
+
+		const int epfd=epoll_create(maxClients);
+		if(epfd<0){
+			perror("epoll_create");
+			return -2;
+		}
+
+		int sock[maxClients];
+		for(int i=0;i<maxClients;i++){
+			sock[i]=getSocket(host,port);
+			setNonblocking(sock[i]);
+//	        fprintf ( stderr, "... socket handle created ... \n" );
+		}
+		struct epoll_event events[maxClients];
+		for(int i=0;i<maxClients;i++){
+			struct epoll_event ev;
+			memset(&ev,0,sizeof ev);
+			ev.events= EPOLLIN|EPOLLOUT|EPOLLET;
+			ev.data.fd=sock[i];
+			if(epoll_ctl(epfd,EPOLL_CTL_ADD,sock[i],&ev)<0)throw"epoll_ctl";
+		}
+		size_t reads{0};
+		size_t writes{0};
+		while(1){
+			const int nfd=epoll_wait(epfd,events,maxClients,-1);
+			for(int i=0;i<nfd;++i){
+				int client=events[i].data.fd;
+				if(events[i].events&EPOLLOUT){
+//					puts("  *** write");
+					writes++;
+					char message[1024];
+					snprintf(message,sizeof message,"GET %s HTTP/1.1\r\nHost: %s:%d\r\n\r\n",requestURI,host,port);
+					const int write_len=write(client,message,strlen(message));
+//					printf("write_len=%d\n",write_len);
+//					printf("%s",message);
+				}
+				if(events[i].events&EPOLLIN){
+//					puts("  *** read");
+					reads++;
+					char message[1024];
+					memset(message,0,sizeof message);
+					int read_len=read(client,message,sizeof message);
+//					printf("read_len=%d\n",read_len);
+//					struct epoll_event ev;
+//					epoll_ctl(epfd,EPOLL_CTL_DEL,client,&ev);
+//					close(client);
+//
+//					int newSock=getSocket(host,port);
+//					setNonblocking(newSock);
+//
+//					memset(&ev,0,sizeof ev);
+//					ev.events= EPOLLIN|EPOLLOUT|EPOLLET;
+//					ev.data.fd=newSock;
+//					if(epoll_ctl(epfd,EPOLL_CTL_ADD,newSock,&ev)<0)throw"epoll_ctl";
+				}
+				printf(" writes: %zu    reads: %zu \n",writes,reads);
+			}
+		}
+		return 0;
+	}
+private:
+	int getSocket(const char*hostname,const int portNumber){
+		int sock=socket(AF_INET,SOCK_STREAM,0);
+		if(sock<0)throw"socket";
+
+		struct sockaddr_in addr;
+		bzero((char*)&addr,sizeof addr);
+
+		struct hostent*he;
+		if((he=gethostbyname(hostname))==NULL)throw"nosuchhost";
+		bcopy(he->h_addr,&addr.sin_addr,he->h_length);
+		addr.sin_family=AF_INET;
+		addr.sin_port=htons(portNumber);
+
+		if(connect(sock,(struct sockaddr*)&addr,sizeof addr)<0)throw"connect";
+
+		return sock;
+	}
+	void setNonblocking(const int sock){
+	    int flag=fcntl(sock,F_GETFL,0);
+	    fcntl(sock,F_SETFL,flag|O_NONBLOCK );
+	    //? verifyok
+	}
+
+
 };
 }
