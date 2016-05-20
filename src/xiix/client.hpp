f380d7e85e3012726bc35345593588d628907146
calin tenitchi <calin.tenitchi@gmail.com>
2016-05-20 21:06:50 +0700

    .

--- src/xiix/client.hpp
+++ src/xiix/client.hpp
@@ -31,20 +31,25 @@ private:
 		conf::print_traffic=ag.hasoption('p');
 		conf::print_stats=ag.hasoption('s');
 		if(ag.hasoption('q'))conf::print_content=false;
+		const bool repeatmode=ag.hasoption('r');
 		if(ag.hasoption('v')){
-			printf("\n   x i i x   m a r k\n\n");
-			printf("      clients: %d\n",nsocks);
-			if(port!=80)printf("          url: http://%s:%d%s\n",hostname,port,uri);
-			else        printf("          url: http://%s%s\n",hostname,uri);
+			printf("\n  x i i x   m a r k\n");
+			if(port!=80)printf("           url: http://%s:%d%s\n",hostname,port,uri);
+			else        printf("           url: http://%s%s\n",hostname,uri);
+			printf("       clients: %d\n",nsocks);
+			printf("        repeat: %s\n",repeatmode?"yes":"no");
+			printf(" print traffic: %s\n",conf::print_traffic?"yes":"no");
+			printf("   print stats: %s\n",conf::print_stats?"yes":"no");
+			printf("   print reply: %s\n",conf::print_content?"yes":"no");
+			puts("");
 		}
 		const int epollfd=epoll_create(nsocks);
 		if(epollfd<0)throw"epoll_create";
 
 		lst<sock*>socks;
 		for(int i=0;i<nsocks;i++)socks.add(new sock(epollfd,hostname,port));
-		const bool repeatmode=ag.hasoption('r');
 		socks.foreach2([repeatmode,uri](sock*s){s->seturi(uri).setrepeatmode(repeatmode).connect();return true;});
-		struct epoll_event evs[nsocks];
+		struct epoll_event*evs=(epoll_event*)calloc(nsocks,sizeof(struct epoll_event));
 		while(1){
 			const int n=epoll_wait(epollfd,evs,nsocks,-1);
 			for(int i=0;i<n;++i){
@@ -56,14 +61,17 @@ private:
 				}catch(const char*msg){
 					if(!strcmp("close",msg)||!strcmp("responsecode",msg)){
 						delete sk;
-						if(meters::socks==0)
+						if(meters::socks==0){
+							free(evs);
 							return 0;
+						}
 					}else{
 						puts("*** exception caught");
 						puts(msg);
 					}
 				}catch(...){
 					puts("*** exception caught");
+					free(evs);
 					return-1;
 				}
 				if(conf::print_stats)printf("   requests: %zu  events: %zu  writes: %zu    reads: %zu \r",meters::requests,meters::events,meters::writes,meters::reads);
